0b74e459d6ffe870634830ce92511ec7a88826f4
Ben Sutherland <taynaron@gmail.com>
2021-01-28 05:55:24 -0800

    add m4a support make writing ... optional use fstrings add song names to output reformat with black

--- lyrics2mp3.py
+++ lyrics2mp3.py
@@ -7,26 +7,31 @@ import taglib
 import time
 import random
 
-'''
-Lyrics2mp3 
-1. Scans directory for mp3 files.
+"""
+Lyrics2mp3
+1. Scans directory for music files.
 2. Searches lyrics for each song on azlyrics.com
-3. Inserts lyrics into mp3 file, or '...' if not found
-'''
+3. If 'write_on_not_found', inserts lyrics into music file, or '...' if not found
+"""
 
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Windows x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/62.0.3239.84 Safari/537.36'}
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Windows x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/62.0.3239.84 Safari/537.36"
+}
 
 
-search_url = 'http://search.azlyrics.com/search.php?q='
+search_url = "http://search.azlyrics.com/search.php?q="
 
-parser = argparse.ArgumentParser(description='lyrics2mp3: Find lyrics and add them into mp3 files')
+parser = argparse.ArgumentParser(
+    description="lyrics2mp3: Find lyrics and add them into music files"
+)
 parser._action_groups.pop()
 
-required = parser.add_argument_group('required arguments')
-optional = parser.add_argument_group('optional arguments')
+required = parser.add_argument_group("required arguments")
+optional = parser.add_argument_group("optional arguments")
 
-required.add_argument('--dir', nargs=1, help='Directory to search for mp3 files')
+required.add_argument("--dir", nargs=1, help="Directory to search for music files")
+optional.add_argument('--write_on_not_found', action='store_true')
 
 args = parser.parse_args()
 
@@ -35,7 +40,7 @@ if args.dir is None:
     sys.exit(0)
 
 if args.dir[0] is None or not os.path.isdir(args.dir[0]):
-    print('Directory "%s" not found.' % args.dir[0])
+    print(f'Directory "{args.dir[0]}" not found.')
     sys.exit(1)
 
 directory = args.dir[0]
@@ -44,55 +49,66 @@ directory = args.dir[0]
 def parse_single_song(href):
     time.sleep(random.randint(1, 9))
 
-    resp = requests.get(url=href, headers=headers,)
+    resp = requests.get(
+        url=href,
+        headers=headers,
+    )
     soup = BeautifulSoup(resp.text, "html.parser")
-    parsed_lyrics = soup.find("div", class_="col-xs-12 col-lg-8 text-center").contents[16].get_text()
+    parsed_lyrics = (
+        soup.find("div", class_="col-xs-12 col-lg-8 text-center")
+        .contents[16]
+        .get_text()
+    )
 
     return parsed_lyrics
 
 
 def parse_azlyrics(html, artist, title):
-    soup = BeautifulSoup(html, "html.parser" )
+    soup = BeautifulSoup(html, "html.parser")
     found_td = soup.find("td", class_="text-left")
+
+    ret = '...' if args.write_on_not_found else None
 
     href = None
     if found_td is not None:
         try:
-            found_artist_name = found_td.find_all('b')[1].text.lower()
-            found_title = found_td.find('b').text.lower()
+            found_artist_name = found_td.find_all("b")[1].text.lower()
+            found_title = found_td.find("b").text.lower()
         except IndexError as e:
-            print('Whatever man, I gave up years ago:', e)
-            return '...'
+            print("Whatever man, I gave up years ago: ", e)
+            return ret
 
         if artist == found_artist_name or title == found_title:
-            href = found_td.find('a')['href']
+            href = found_td.find("a")["href"]
         else:
-            print('Wrong lyrics found:', artist, ' vs ', found_artist_name, ' skipping')
-            return '...'
+            print(f"Wrong lyrics found: {artist} vs {found_artist_name}, skipping")
+            return ret
     else:
-        print('Lyrics not found on site :(')
-        return '...'
+        print(f'Lyrics not found on site for "{title}" :(')
+        return ret
 
-    print('href:', href)
+    print("href: ", href)
 
     try:
         parsed_lyrics = parse_single_song(href)
     except Exception as e:
-        print('not found:', e)
-        return '...'
+        print(f'not found for "{title}": ', e)
+        return ret
 
-    print('parsed lyrics!')
+    print("parsed lyrics!")
     return parsed_lyrics
 
 
 def get_lyrics(artist, title):
-    url = search_url + artist + ' ' + title
+    url = f"{search_url}{artist} {title}"
 
     time.sleep(1)
 
-    resp = requests.get(url=url, headers=headers, )
+    resp = requests.get(
+        url=url,
+        headers=headers,
+    )
     parsed_lyrics = parse_azlyrics(resp.text, artist=artist, title=title)
-
     return parsed_lyrics
 
 
@@ -100,23 +116,23 @@ for dir_path, dirs, files in os.walk(directory):
     path = dir_path.split(os.sep)
     for file in files:
         ext = os.path.splitext(file)[-1].lower()
-        if ext == ".mp3":
+        if ext in (".mp3", ".m4a"):
             file_path = os.path.join(dir_path, file)
 
             audiofile = taglib.File(file_path)
-            old_lyrics = audiofile.tags.get('LYRICS', [''])[0]
+            old_lyrics = audiofile.tags.get("LYRICS", [""])[0]
 
-            if (old_lyrics is not None and len(old_lyrics) > 10) or old_lyrics == '...':
-                print('lyrics found in mp3 file: ', file_path, ' skipping')
+            if old_lyrics is not None and len(old_lyrics) > 10 or args.write_on_not_found and old_lyrics == '...':
+                print(f"lyrics found in music file: {file_path} skipping")
                 continue
             try:
-                search_artist = audiofile.tags['ARTIST'][0].lower()
-                search_title = audiofile.tags['TITLE'][0].lower()
+                search_artist = audiofile.tags["ARTIST"][0].lower()
+                search_title = audiofile.tags["TITLE"][0].lower()
             except KeyError:
-                print('no artist or title in mp3 file')
+                print("no artist or title in music file")
                 continue
 
             lyrics = get_lyrics(artist=search_artist, title=search_title)
             if lyrics is not None:
-                audiofile.tags['LYRICS'] = lyrics
+                audiofile.tags["LYRICS"] = lyrics
                 audiofile.save()
